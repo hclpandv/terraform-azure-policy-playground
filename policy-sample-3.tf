d0d449a77e0655614c82c7c20a889d498ca4971c
hclpandv <vikas110@gmail.com>
2022-11-03 00:28:06 +0800

    All 3 policy samples tested

--- policy-sample-3.tf
+++ policy-sample-3.tf
@@ -1,17 +1,24 @@
 #-------------------------------------
 # Built-In Policy assignment to RG
 #-------------------------------------
-resource "azurerm_resource_group_policy_assignment" "built_in_assignment" {
-  name                 = "pol-ass-built_in_allowed-resource-locations"
+resource "azurerm_resource_group_policy_assignment" "built_in_assignment_01" {
+  name                 = "pol-ass-built-in-allowed-virtual-machine-size-SKUs"
   resource_group_id    = data.azurerm_resource_group.main.id
-  policy_definition_id = local.policy_definition_map.Allowed_locations
-  #policy_definition_id = data.azurerm_policy_definition.allowed_locations_policy_definitions.id
+  policy_definition_id = local.policy_definition_map.Allowed_virtual_machine_size_SKUs
+  parameters = jsonencode({
+      listOfAllowedSKUs = {
+        "value": ["standard_b2ms","standard_b2s","standard_d2as_v4"]
+      }
+  })
+}
 
-  parameters = <<PARAMS
-    {
-      "listOfAllowedLocations": {
+resource "azurerm_resource_group_policy_assignment" "built_in_assignment_02" {
+  name                 = "pol-ass-built-in-allowed-resource-locations"
+  resource_group_id    = data.azurerm_resource_group.main.id
+  policy_definition_id = data.azurerm_policy_definition.allowed_locations_policy_definitions.id
+  parameters = jsonencode({
+      listOfAllowedLocations = {
         "value": ["westeurope","northeurope"]
       }
-    }
-PARAMS
+  })
 }
